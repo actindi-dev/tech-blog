4e45bc726dce5adaf743fd5548119f5c2d31fb3e
CHIBA Masaomi <mc@setq.(none)>
2009-12-21 18:56:44 +0900

    不要コード整理

--- tech.actindi.net.lisp
+++ tech.actindi.net.lisp
@@ -4,21 +4,6 @@
 (defvar *entries-per-page* 2)
 (defparameter *default-directory* (directory-namestring *load-truename*))
 
-;; onlisp
-(defun group (source n)
-  "groups every n elements together into new sublists.
-   e.g. (group '(1 a 2 b) 2) -> ((1 a) (2 b))"
-  (when (zerop n) (error "zero length"))
-  (labels ((rec (source acc)
-	     (let ((rest (nthcdr n source)))
-	       (if (consp rest)
-		   (rec rest (cons (subseq source 0 n) acc))
-		   (nreverse (cons source acc))))))
-    (if source
-	(rec source nil)
-	nil)))
-
-
 (defun number-to-kanji (num)
   (let ((digit   #("零" "一" "二" "三" "四" "五" "六" "七" "八" "九"))
         (subunit #("" "十" "百" "千"))
@@ -38,133 +23,6 @@
           do (setf num n)
           finally (return (apply #'concatenate (cons 'string (reverse result)))))))
 
-
-#|
-(defstruct (entry (:type list))
-  path
-  title
-  body
-  author
-  date
-  category
-  )
-|#
-
-
-;; (defmacro defentry (path (&key (title "")
-;;                                (author "")
-;;                                (date (get-universal-time))
-;;                                (category ""))
-;;                     &body body)
-;;   (let ((ent (gensym))
-;;         (e (gensym)))
-;;     `(let ((,ent (find ,path *all-entries* :key #'entry-path :test #'string=)))
-;;        (cond (,ent (setf (entry-body ,ent)
-;;                          (with-html-output-to-string (out nil :indent T) ,@body))
-;;                    (setf (entry-title ,ent)
-;;                          ,title)
-;;                    (setf (entry-author ,ent)
-;;                          ,author)
-;;                    (setf (entry-date ,ent)
-;;                          ,date)
-;;                    (setf (entry-category ,ent)
-;;                          ,category))
-;;              (T (let ((,e (make-entry :path ,path
-;;                                       :title ,title
-;;                                       :author ,author
-;;                                       :date ,date
-;;                                       :category ,category
-;;                                       :body (with-html-output-to-string (out nil :indent T) ,@body))))
-;;                   (push ,e *all-entries*)
-;;                   (define-easy-handler (,(intern path) :uri ,path) ()
-;;                       (setf (content-type*) "text/html; charset=utf-8")
-;;                       (with-html-output-to-string (out nil :indent T :prologue T)
-;;                         ((:html :xmlns "http://www.w3.org/1999/xhtml")
-;;                          (:head ((:meta :http-equiv "Content-Type" :content "text/html; charset=utf-8"))
-;;                                 (:title "アクトインディ技術部隊報告書")
-;;                                 ((:link :href "http://tech.actindi.net/stylesheets/reset.css" :rel "stylesheet" :type "text/css"))
-;;                                 ((:link :href "http://tech.actindi.net/stylesheets/basic.css" :rel "stylesheet" :type "text/css"))
-;;                                 ((:link :href "/rss.xml" :rel "alternate" :type "application/rss+xml" :title "Actindi Tech blog")))
-;;                          (:body
-;;                           ((:div :id "header")
-;;                            ((:div :class "inner")
-;;                             ((:div :class "twitter")
-;;                              ((:h2 :class "design") "twitter")
-;;                              (:p "こんにちは!!、こんにちは!!、こんにちは!!"))
-;;                             ((:h1 :class "design_a")
-;;                              ((:a :href "" :title "") "タイトル"))
-;;                             ((:ol :id "bread_crumbs")
-;;                            (:li ((:a :href "/") "home"))
-;;                            #|(:li ((:a :|href| "" :|title| "") "home"))|#
-;;                            #|(:li "1234")|#)))
-;;                           ((:div :class "inner")
-;;                          ((:div :id "columns")
-;;                           ;; contents
-;;                           ((:div :id "cotents")
-;;                            ((lambda (x)
-;;                               (with-html-output (out nil :indent T)
-;;                                 ((:div :class "content")
-;;                                  (:h2
-;;                                   ((:a :href (entry-path x))
-;;                                    (princ (entry-title x) out)))
-;;                                  ((:dl :class "date")
-;;                                   (:dd (format-date out
-;;                                                     "%g%#e年%#m月%#d日(%v) %H時%M分%S秒"
-;;                                                     (entry-date x)))
-;;                                   (:dt "区分")
-;;                                   (:dd (princ (entry-category x) out))
-;;                                   (:dt "報告者: ")
-;;                                   (:dd (princ (entry-author x) out)))
-;;                                  (:p (princ (entry-body x) out))
-;;                                  ((:p :class "to_top")
-;;                                   ((:a :href "/") "トップページに戻る")))))
-;;                             ,e)
-;;                            ((:div :class "footer")
-;;                             "<div id=\"disqus_thread\"></div><script type=\"text/javascript\" src=\"http://disqus.com/forums/actindi/embed.js\"></script><noscript><a href=\"http://actindi.disqus.com/?url=ref\">View the discussion thread.</a></noscript><a href=\"http://disqus.com\" class=\"dsq-brlink\">blog comments powered by <span class=\"logo-disqus\">Disqus</span></a>"))
-;;                           ((:div :id "local_nav")
-;;                            ((:div :id "counter")
-;;                             (:dl
-;;                              (:dt "本頁の来客数")
-;;                              (:dd (format out
-;;                                           "~A名"
-;;                                           (number-to-kanji (incf-counter))))))
-;;                            #|(:princ *top-member*)|#))
-;;                          ;; ads
-;;                          ((:div :id "foo_ads")
-;;                           ((:div :id "categories")
-;;                            ((:h2 :class "design") "カテゴリー")
-;;                            (:ul
-;;                             (mapc (lambda (x)
-;;                                     (with-html-output (out nil :indent T)
-;;                                       (:li ((:a :href (entry-path x))
-;;                                             (princ (entry-title x) out)))))
-;;                                   *all-entries*))
-;;                            ((:p :class "to_actindi")
-;;                             ((:a :href "" :title "") "アクトインディ")))
-;;                           ;; poster
-;;                           ((:div :class "poster")
-;;                            ((:img :src "images/poster_01.jpg" :alt "aaaa")))))))))))))))
-;; 
-;; (defmacro defentry-ut ((year month date hour minute second &optional time-zone)
-;;                        (&key (title "")
-;;                              (author "")
-;;                              (category ""))
-;;                        &body body)
-;;   (let ((d (encode-universal-time
-;;             second
-;;             minute
-;;             hour
-;;             date
-;;             month
-;;             year
-;;             time-zone)))
-;;     `(defentry ,(format nil "/~A" d)
-;;          (:title ,title
-;;                  :author ,author
-;;                  :category ,category
-;;                  :date ,d)
-;;        ,@body)))
-
 ;; CSS
 (define-easy-handler (reset.css :uri "/stylesheets/reset.css") ()
   (setf (content-type*) "text/css")
@@ -175,7 +33,6 @@
   (setf (content-type*) "text/css")
   (kmrcl:read-file-to-string
    (merge-pathnames "basic.css" *default-directory*)))
-
 
 ;; RSS
 (define-easy-handler (|/rss.xml| :uri "/rss.xml") ()
@@ -199,7 +56,7 @@
                  (format-date out "<pubDate>%a, %d %b %Y %H:%M:%S +0900</pubDate>" (entry-date x)))))
             (get-all-entries))))))
 
-(setq *dispatch-table* '(DISPATCH-EASY-HANDLERS DEFAULT-DISPATCHER))
+(setq *dispatch-table* '(dispatch-easy-handlers default-dispatcher))
 
 ;; メンバー一覧
 (defparameter *top-member*
@@ -291,74 +148,6 @@
           "//]]>"))))
       ((:div :id "footer")
        ((:p :class "center") "Copyright &copy; 2009 アクトインディ All rights reserved.")))))
-
-;; テンプレ
-#|(defmacro define-actindi.net-template ((name path) (&rest args) contents)
-`(define-easy-handler (,name :uri ,path) ,args
-(with-html-output-to-string (out nil :indent T :prologue T)
-  ((:html :xmlns "http://www.w3.org/1999/xhtml")
-   (:head ((:meta :http-equiv "Content-Type" :content "text/html; charset=utf-8"))
-          (:title "アクトインディ技術部隊報告書")
-          ((:link :href "http://tech.actindi.net/stylesheets/reset.css" :rel "stylesheet" :type "text/css"))
-          ((:link :href "http://tech.actindi.net/stylesheets/basic.css" :rel "stylesheet" :type "text/css"))
-          #|((:link :href "/stylesheets/reset.css" :rel "stylesheet" :type "text/css"))|#
-          #|((:link :href "/stylesheets/basic.css" :rel "stylesheet" :type "text/css"))|#
-          ((:link :href "/rss.xml" :rel "alternate" :type "application/rss+xml" :title "Actindi Tech blog")))
-   (:body
-    ((:div :id "header")
-     ((:div :class "inner")
-      ((:div :class "twitter")
-       ((:h2 :class "design") "twitter")
-       (:p "こんにちは!!、こんにちは!!、こんにちは!!"))
-      ((:h1 :class "design_a")
-       ((:a :href "" :title "") "タイトル"))
-      ((:ol :id "bread_crumbs")
-       (:li ((:a :href "/") "home")))))
-    ((:div :class "inner")
-     ((:div :id "columns")
-      ((:div :id "cotents")
-       ;; content
-       ,contents
-       ((:div :class "footer")))
-      ((:div :id "local_nav")
-       ;; news
-       (princ *block-news* out)
-       ((:div :id "counter")
-        (:dl
-         (:dt "本頁の来客数")
-         (:dd (format out
-                      "~A名"
-                      (number-to-kanji (incf-counter))))))
-       (princ *top-member* out)))
-     ;; ads
-     ((:div :id "foo_ads")
-      ((:div :id "categories")
-       ((:h2 :class "design") "カテゴリー")
-       (:ul
-        (mapc (lambda (x)
-                (with-html-output (out nil :indent T)
-                  (:li ((:a :href (entry-path x))
-                        (princ (entry-title x) out)))))
-              (get-all-entries)))
-       ((:p :class "to_actindi")
-        ((:a :href "" :title "") "アクトインディ")))
-      ((:div :class "poster")
-       ((:img :src "images/poster_01.jpg" :alt "aaaa")))
-      ((:script :type "text/javascript")
-       "//<![CDATA["
-       "(function() {
-		var links = document.getElementsByTagName('a');
-		var query = '?';
-		for(var i = 0; i < links.length; i++) {
-			if(links[i].href.indexOf('#disqus_thread') >= 0) {
-				query += 'url' + i + '=' + encodeURIComponent(links[i].href) + '&';
-			}
-		}
-		document.write('<script charset=\"utf-8\" type=\"text/javascript\" src=\"http://disqus.com/forums/actindi/get_num_replies.js' + query + '\"></' + 'script>');
-	})();"
-       "//]]>"))))
-   ((:div :id "footer")
-    ((:p :class "center") "Copyright &copy; 2009 アクトインディ All rights reserved."))))))|#
 
 (defmacro define-actindi.net-template ((name path) (&rest args) contents)
   `(define-easy-handler (,name :uri ,path) ,args
@@ -616,7 +405,6 @@
           (entry-category entry) category
           (entry-body entry) body)
     (hunchentoot:redirect path)))
-
 
 (defvar *server*)
 
