6fe571e61b4c58621bcbbd70b86a4406a1fd1389
CHIBA Masaomi <mc@setq.(none)>
2009-12-21 18:45:26 +0900

    簡単に起動できるファイルを作成

--- quick-start.lisp
+++ quick-start.lisp
@@ -1,11 +1,14 @@
-;; (pushnew :quick-start *features*)
+(asdf:oos 'asdf:load-op :tech.actindi.net)
 
-#+quick-start
-(progn
-  (defparameter *site-files-directory* #p"/var/www/tech-blog/")
-  (asdf:oos 'adsf:load-op :tech.actindi.net)
-
+;; ファイルの置かれているディレクトリを設定
+(let ((*default-pathname-defaults* (make-pathname 
+                                    :directory
+                                    (directory-namestring *load-pathname*))))
   ;; start
   (tech.actindi.net:start-tech.actindi.net)
-  (load (merge-pathnames "migrate" *site-files-directory*))
-  (load (merge-pathnames "blog" *site-files-directory*)))
+
+  ;; 過去の記事を読み込むユーティリティ
+  (load "migrate")
+  
+  ;; 過去の記事を読み込み
+  (load "blog"))
